d6845d386669540ed35ad364693e80c7faf40c95
曹晟原 <62429807+Pandaas-cn@users.noreply.github.com>
2020-04-01 22:50:02 +0800

    Update views.py
    Fixed an issue that could cause data not to be saved to Excel---Try is used incorrectly

--- autoscore/views.py
+++ autoscore/views.py
@@ -5,14 +5,13 @@ from django.http import FileResponse
 
 # Create your views here.
 
-EXCLE_FILE = r'F:\Django_projects\Auto_score_wjw\autoscore\score.xlsx'
-SAVE_FILE = r'F:\Django_projects\Auto_score_wjw\autoscore\savescore.xlsx'
+EXCLE_FILE = r'C:\Python_web\Auto_score_wjw\autoscore\score.xlsx'
+SAVE_FILE = r'C:\Python_web\Auto_score_wjw\autoscore\savescore.xlsx'
 SUBJECT_LIST = ['chinese','math','english','wuli','huaxue','shengwu']
-ERROR_FILE = r'F:\Django_projects\Auto_score_wjw\autoscore\error.txt'
+ERROR_FILE = r'C:\Python_web\Auto_score_wjw\autoscore\error.txt'
 NAME_LIST = ['金筱易', '徐嘉孺', '冯国申', '杨书勤', '王建为', '宋祎垌', '王天骏', '曹佳硕', '王希蒙', '穆玥彤', '栾博业', '王禹璇', '金云鹏', '张靖悦', '赵益铭', '解丰硕', '滕金楠', '赵一博', '尧洁', '董钰鑫', '张馨童', '李思佳', '许新敏', '刘佳欣', '陈冬妮', '英瑞桐', '康汝佳怡', '张田佳含', '郑荟竹', '王奥涵', '石成功', '于世畔', '刘一田', '赵津磊', '白长健', '高艺鸣', '付晓玮', '李宇梁', '沈春潇', '李昶杰', '程铄茜', '沈凯文', '杨箫宁', '杨思齐', '刘宇凡', '薛宇轩', '周艺卓',]
 NAME_DICT = dict.fromkeys(NAME_LIST,False)
 data = pd.DataFrame(pd.read_excel(EXCLE_FILE))
-
 def setdeafult(request):
     global NAME_DICT
     global data
@@ -23,7 +22,7 @@ def setdeafult(request):
         NAME_DICT = dict.fromkeys(NAME_LIST, False)
         data = pd.DataFrame(pd.read_excel(EXCLE_FILE))
         try:
-            data.to_excel(SAVE_FILE)
+            data.to_excel(SAVE_FILE,index=False)
         except Exception as e:
             with open(ERROR_FILE,'wt') as f:
                 info = str(time.asctime()) + str(e) + '\n'
@@ -33,37 +32,52 @@ def setdeafult(request):
     else:
         return render(request,'setdefault_false.html')
 
+
 def index(request):
     if request.method == 'GET':
         return render(request,'auto_score.html')
     else:
-        print(request.POST)
+        with open('log.txt','at')as f:
+            temp_info = str(request.POST) + '\n'
+            f.write(temp_info)
         username = request.POST.get('username')
         if username not in NAME_LIST:
             return render(request,'namenotexist.html')
-        NAME_DICT[username] = True
         chinese = request.POST.get('chinese')
         math = request.POST.get('math')
         english = request.POST.get('english')
         wuli = request.POST.get('wuli')
         huaxue = request.POST.get('huaxue')
         shengwu = request.POST.get('shengwu')
+        lizong = request.POST.get('lizong')
         try:
-            data['chinese'][data['Name'] == username] = float(chinese)
-            data['math'][data['Name'] == username] = float(math)
-            data['english'][data['Name'] == username] = float(english)
-            data['wuli'][data['Name'] == username] = float(wuli)
-            data['huaxue'][data['Name'] == username] = float(huaxue)
-            data['shengwu'][data['Name'] == username] = float(shengwu)
-        except ValueError as e:
-            pass
-        try:
-            data.to_excel(SAVE_FILE)
+            if chinese:
+                data['chinese'][data['Name'] == username] = float(chinese)
+            if math:
+                data['math'][data['Name'] == username] = float(math)
+            if english:
+                data['english'][data['Name'] == username] = float(english)
+            if wuli:
+                data['wuli'][data['Name'] == username] = float(wuli)
+            if huaxue:
+                data['huaxue'][data['Name'] == username] = float(huaxue)
+            if shengwu:
+                data['shengwu'][data['Name'] == username] = float(shengwu)
+            if lizong:
+                data['lizong'][data['Name'] == username] = float(lizong)
         except Exception as e:
             with open(ERROR_FILE,'wt') as f:
                 info = str(time.asctime()) + str(e) + '\n'
                 f.write(info)
             return render(request,'contact_admin.html')
+        try:
+            data.to_excel(SAVE_FILE,index = False)
+        except Exception as e:
+            with open(ERROR_FILE,'wt') as f:
+                info = str(time.asctime()) + str(e) + '\n'
+                f.write(info)
+            return render(request,'contact_admin.html')
+        NAME_DICT[username] = True
         return render(request,'sucess.html')
 
 
